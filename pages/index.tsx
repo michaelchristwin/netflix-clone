5b9c1dd36e8d52cbbff68ea8ba4861cf1a39986a
michaelchristwin <kelechukwuchristwin@gmail.com>
2023-02-17 10:50:52 +0100

    Refactored

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ function Home(): JSX.Element {
     <main>
       <section className="nt-bg">
         <div className="flex justify-between pt-7">
-          <p className="text-red-700 text-5xl font-extrabold ml-10">NETFLIX</p>
+          <p className="text-red-600 text-5xl font-extrabold ml-10">NETFLIX</p>
           <button className="bg-red-600 text-white w-20 rounded h-9 px-2 float-right mr-8">
             Sign in
           </button>
@@ -15,6 +15,20 @@ function Home(): JSX.Element {
           <p className="text-center block watch">
             Watch anywhere. Cancel anytime.
           </p>
+          <p className="ready text-center pt-5 pb-10 font-normal">
+            Ready to watch? Enter your email to create or restart your
+            membership.
+          </p>
+          <div className="flex email">
+            <input
+              type="text"
+              className="block w-[530px] h-[70px] pl-2 border"
+              placeholder="Email address"
+            />
+            <button className="block bg-red-600 px-2 w-[244px] hover:bg-[#f40612] btn">
+              Get Started
+            </button>
+          </div>
         </div>
       </section>
     </main>
